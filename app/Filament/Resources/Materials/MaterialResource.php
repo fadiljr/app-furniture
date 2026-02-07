507914e039f3989e3ea2e3355990c86c6cc78df3
patengtot <fahri.fajariadi@gmail.com>
2026-02-08 01:08:27 +0700

    ubah icon

--- app/Filament/Resources/Materials/MaterialResource.php
+++ app/Filament/Resources/Materials/MaterialResource.php
@@ -18,7 +18,7 @@ class MaterialResource extends Resource
 {
     protected static ?string $model = Material::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::WrenchScrewdriver;
 
     protected static ?string $recordTitleAttribute = 'Material';
 
--- app/Filament/Resources/Projects/ProjectResource.php
+++ app/Filament/Resources/Projects/ProjectResource.php
@@ -19,7 +19,7 @@ class ProjectResource extends Resource
 {
     protected static ?string $model = Project::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::PresentationChartBar;
 
     protected static ?string $recordTitleAttribute = 'Project';
 
