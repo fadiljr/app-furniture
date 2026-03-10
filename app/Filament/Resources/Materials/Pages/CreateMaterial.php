48b2f8561f883b959f66f3f8780c48c384a617fb
patengtot <fahri.fajariadi@gmail.com>
2026-03-10 23:50:23 +0700

    create and edit back to index

--- app/Filament/Resources/Materials/Pages/CreateMaterial.php
+++ app/Filament/Resources/Materials/Pages/CreateMaterial.php
@@ -8,4 +8,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateMaterial extends CreateRecord
 {
     protected static string $resource = MaterialResource::class;
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
 }
--- app/Filament/Resources/PriceLists/Pages/CreatePriceList.php
+++ app/Filament/Resources/PriceLists/Pages/CreatePriceList.php
@@ -8,4 +8,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreatePriceList extends CreateRecord
 {
     protected static string $resource = PriceListResource::class;
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
 }
